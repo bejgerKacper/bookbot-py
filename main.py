52a9132663b550d71a08a7948a790822202a0e86
bejgerKacper <149320181+bejgerKacper@users.noreply.github.com>
2023-12-04 15:52:13 +0100

    add print_report() and refactor

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
-def count_letters(string_file):
-    letter_dictionary = {}
-    for letter in string_file:
+def print_report(words, char_dictionary, book_path):
+    print(f"--- Begin raport of book {book_path} ---")
+    print(f"{words} words found in the document")
+    chars = dictionary_to_list(char_dictionary)
+    chars.sort()
+    for char in chars:
+        print(f"The {char} character was found {char_dictionary[char]} times")
+    print("--- End raport ---")
+
+def dictionary_to_list(char_dictionary):
+    char_list = []
+    for char in char_dictionary:
+        if char.isalpha():
+            char_list.append(char)
+    return char_list
+
+def count_letters(text_file):
+    char_dictionary = {}
+    for letter in text_file:
         lowered = letter.lower()
-        if lowered in letter_dictionary:
-            letter_dictionary[lowered] += 1
+        if lowered in char_dictionary:
+            char_dictionary[lowered] += 1
         else:
-            letter_dictionary[lowered] = 1
-    print(letter_dictionary)
+            char_dictionary[lowered] = 1
+    return char_dictionary
 
 def open_file(file_path):
     with open(file_path) as f:
         return f.read()
 
-def split_text(string_file): 
-    return len(text_file.split())
+def count_words(string_file): 
+    return len(string_file.split())
 
 def main():
-    string_file = open_file("/Users/kacper-bejger/workspace/github.com/bejgerKacper/bookbot-py/books/frankenstein.txt")
-    # words = split_text(text_file)
-    # print(words)
-    count_letters(string_file)
+    book_path = "books/frankenstein.txt"
+    text_file = open_file(book_path)
+    words_counted = count_words(text_file)
+    char_dictionary = count_letters(text_file)
+    print_report(words_counted, char_dictionary, book_path)
 
 main()
