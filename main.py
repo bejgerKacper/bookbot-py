88bf63924d7f6f835c3d6855c4a94e27cd4101d4
bejgerKacper <149320181+bejgerKacper@users.noreply.github.com>
2023-12-04 14:21:43 +0100

    count letters in the given string function

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-def count_letters(text_file):
+def count_letters(strings):
+    letter_dictionary = {}
+    for letter in strings:
+        if letter in letter_dictionary:
+            letter_dictionary[letter] += 1
+        else:
+            letter_dictionary[letter] = 1
+    print(letter_dictionary)
 
 def open_file(file_path):
     with open(file_path) as f:
         text_file = f.read()
     return text_file
 
-def split_text(text_file):
+def split_text(text_file): 
     words = text_file.split()
     return len(words)
 
 def main():
     text_file = open_file("/Users/kacper-bejger/workspace/github.com/bejgerKacper/bookbot-py/books/frankenstein.txt")
-    words = split_text(text_file)
-    print(words)
+    # words = split_text(text_file)
+    # print(words)
+    count_letters(text_file)
 
 main()
