0f8600742a4f4213c630b258c90b9e617666d527
bejgerKacper <149320181+bejgerKacper@users.noreply.github.com>
2023-12-04 16:37:54 +0100

    refactor count_letters() to get_chars_dict()

--- main.py
+++ main.py
@@ -15,6 +15,17 @@ def get_words_count(txt_file):
 def get_lower_chars(txt_file):
     return txt_file.lower()
 
+#total count of every single character in txt_file as [char]:count
+def get_chars_dict(txt_file):
+    lower_chars = get_lower_chars(txt_file)
+    chars_dict = {}
+    for char in lower_chars:
+        if char in chars_dict:
+            chars_dict[char] += 1
+        else:
+            chars_dict[char] = 1
+    return chars_dict
+
 def print_report(words, char_dictionary, book_path):
     print(f"--- Begin raport of book {book_path} ---")
     print(f"{words} words found in the document")
